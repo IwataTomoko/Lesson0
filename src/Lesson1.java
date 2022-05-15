7708d90a467bce28c0bb1a0c96dc766bb798db7a
IwataTomoko <iwatomo02468@gmail.com>
2022-05-15 20:38:00 +0900

    コメントに句読点を追加

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -1,7 +1,7 @@
 public class Lesson1 {
     public static void main(String[] args) {
 
-        //10回連続でHello worldを出力、11回目からはHelloだけを出力
+        //10回連続でHello worldを出力、11回目からはHelloだけを出力.
 
         for (int i = 0; i < 20; i++) {
             if (i < 11) {
